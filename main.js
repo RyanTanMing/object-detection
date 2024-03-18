d45f04cdc6dae4f0af5175d956beb933e23a9219
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2024-03-18 17:00:09 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 objects=[]
 status=""
 function setup() {
-    canvas=createCanvas(640,420)
+    canvas=createCanvas(380,380)
 canvas.center()
+v=createCapture(VIDEO)
+v.size(380,380)
+v.hide()
 objectdetect=ml5.objectDetector("cocossd",modelloaded)
 }
 function preload() {
     img=loadImage("animals.jpg")
 }
 function draw() {
- image(img,0,0,640,420)
+ image(v,0,0,380,380)
  if (status!="") {
+    objectdetect.detect(v,gotresult)
+    r=random(255)
+    g=random(255)
+    b=random(255)
     for (let index = 0; index < objects.length; index++) {
        document.getElementById('status').innerHTML="object detected"
-       fill("red")
+       document.getElementById('num_of_objects').innerHTML="number of objects detected "+objects.length       
+       fill(r,g,b)
        p=floor(objects[index].confidence*100)
        text(objects[index].label+" "+p+"%",objects[index].x,objects[index].y)
        noFill()
-       stroke("red")
+       stroke(r,g,b)
        rect(objects[index].x,objects[index].y,objects[index].width,objects[index].height)
     }
  }
@@ -25,7 +33,7 @@ function draw() {
 function modelloaded(){
     console.log("model loaded")
     status="true"
-    objectdetect.detect(img,gotresult)
+    objectdetect.detect(v,gotresult)
 }
 function gotresult(error,results) {
     if (error==true) {
